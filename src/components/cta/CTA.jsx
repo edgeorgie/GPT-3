d2fb599cfe62141fe0887294d456b72fa8875ae5
Edwin Jorge <ed.jorge1122@gmail.com>
2022-03-31 08:15:33 -0500

    ADD: CTA section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,6 +3,14 @@ import './Styles.scss'
 
 export const CTA = () => {
   return (
-    <div>CTA</div>
-  )
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <span>Request Early Access to Get Started</span>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 }
